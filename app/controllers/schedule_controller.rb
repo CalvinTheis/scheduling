a003fc6ce5916810312ce86ba0d6889f8b96bea7
Calvin Theis <calvin-theis@proton.me>
2025-01-18 13:24:32 -0600

    backend: schedule: move helpers into proper file

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,36 +1,34 @@
 require "time"
 
-# Controller-local helpers
-module ScheduleHelper
-  # Calculates the height of a box with the given duration.
-  # Used in the css "height" attribute for the block.
-  def height_percent_for_time(duration)
-    100 * (duration - @start_time) / (@end_time - @start_time)
-  end
-
-  # Calculates the offset from the top of the column for the given start time.
-  # Used in the css "top" attribute for the block.
-  def top_percent_for_time(time)
-  time_of_day = time.seconds_since_midnight
-    100 * (time_of_day - @start_time) / (@end_time - @start_time)
-  end
-
-  # Creates formatted time based on seconds since midnight.
-  def format_time(seconds)
-    Time.at(seconds+(60*60*6)).strftime("%H:%M")
-  end
-end
-
 class ScheduleController < ApplicationController
   helper ScheduleHelper
 
   # Constants that define the beginning and end of the period shown on the schedule.
   START_TIME = Time.parse("5:00").seconds_since_midnight
-  END_TIME = Time.parse("20:00").seconds_since_midnight
+  END_TIME = Time.parse("17:00").seconds_since_midnight
 
   def index
     @start_time = START_TIME
     @end_time = END_TIME
-    @technicians = Technician.all()
+    @orders_by_name = {}
+    Technician.select("name").each do |technician|
+      @orders_by_name[technician.name] = get_work_orders_for_technician technician.name
+    end
+  end
+
+  private
+  def get_work_orders_for_technician(technician_name)
+    WorkOrder
+      .joins(:technician, :location)
+      .where("technicians.name = ?", technician_name)
+      .select(
+        "locations.name AS location_name",
+        "city",
+        "time",
+        "duration",
+        "price",
+        "id",
+        "technicians.name AS technician"
+      )
   end
 end
--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,2 +1,19 @@
 module ScheduleHelper
+  # Calculates the height of a box with the given duration.
+  # Used in the css "height" attribute for the block.
+  def height_percent_for_time(duration)
+    100 * (duration*60) / (@end_time - @start_time)
+  end
+
+  # Calculates the offset from the top of the column for the given start time.
+  # Used in the css "top" attribute for the block.
+  def top_percent_for_time(time)
+  time_of_day = time.seconds_since_midnight
+    100 * (time_of_day - @start_time) / (@end_time - @start_time)
+  end
+
+  # Creates formatted time based on seconds since midnight.
+  def format_time(seconds)
+    Time.at(seconds+(60*60*6)).strftime("%H:%M")
+  end
 end
